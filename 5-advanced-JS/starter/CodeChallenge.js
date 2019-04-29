c759aca46293b7b08c24af095775cf820b8b336c
Toua Thao <touathao09@gmail.com>
2019-04-29 17:18:00 -0500

    finish

--- 5-advanced-JS/starter/CodeChallenge.js
+++ 5-advanced-JS/starter/CodeChallenge.js
@@ -24,66 +24,110 @@ we want to type it to the Question prototype properity instead.
 *////////////////
 
 ///now to hide our code. And to stop it from interfering with other programer
-(function() {
+(function () {
 
-
-let Question = function(gameQuestion,answerArray,correctQuestion) {
-    this.gameQuestion = gameQuestion
-    this.answerArray = answerArray
-    this.correctQuestion = correctQuestion
+    let Question = function (gameQuestion, answerArray, correctQuestion) {
+        this.gameQuestion = gameQuestion
+        this.answerArray = answerArray
+        this.correctQuestion = correctQuestion
     }
-    
+
     //This is the question prototype
-    Question.prototype.displayQuestion = function() {
+    Question.prototype.displayQuestion = function () {
         console.log(this.gameQuestion);
         //now we need to loop through the question cause some have 2 question or 3
         for (let i = 0; i < this.answerArray.length; i++) {
-            console.log(i + ': ' +this.answerArray[i])
-            
+            console.log(i + ': ' + this.answerArray[i])
+
         }
     }
-    
+
     //this question prototype would now need to check if the answer is corrected or not
-    Question.prototype.checkAnswer = function (ans){
-    if (ans === this.correctQuestion) {
-        console.log('correct')
-    } else{
-        console.log('Try agian')
+    Question.prototype.checkAnswer = function (ans, callBack) {
+
+        let sc;
+        if (ans === this.correctQuestion) {
+            console.log('correct')
+            //this callBack is to be pass to our score function 
+            sc = callBack(true)
+        } else {
+            console.log('Try agian')
+            sc = callBack(false)
+        }
+        this.displayScore(sc)
     }
+
+    Question.prototype.displayScore = function(score){
+        console.log('Your current score is ' + score)
+        console.log('----------------------------------')
     }
-    
-    
+
+
     //declare the new  question
     let question1 = new Question('what is my name', [
-        'Toua','No'
+        'Toua', 'No'
         //this array will store all the possible answer to the question
     ],
-    //this will contain the correct answer
-    0
+        //this will contain the correct answer
+        0
     )
-    
-    let question2 = new Question('what is the color of the sky?',[
-        'red','blue'
+
+    let question2 = new Question('what is the color of the sky?', [
+        'red', 'blue'
     ],
-    1
+        1
     )
-    
+
     let question3 = new Question('how many wood could a woodchuck', [
-        'one','none','many'
+        'one', 'none', 'many'
     ],
-    2
+        2
     )
-    
-    //store the question in an array
-    let questionArray = [ question1,question2,question3]
-    //we create a randome selector. Then mutiply by our questionArray length
-    let randomSelector = Math.floor(Math.random()* questionArray.length)
-    //now we need to use randomselector on our array and use our displayQuestion from the 
-    //prototype from above
-    questionArray[randomSelector].displayQuestion()
-    
-    //now we need to use parseInt to change what they pick to a number
-    let answerAsk =parseInt( prompt('Please select the correct answer'))
-    
-    questionArray[randomSelector].checkAnswer(answerAsk)
+
+    //write a function that keep the score
+    function score(){
+        let sc = 0;
+        //now we pass a paramenter of correct
+        //this is a boolean
+        return function(correct){
+            if (correct) {
+                sc ++
+            }
+            return sc
+        }
+    }
+
+    let keepScore = score();
+
+
+    let questionArray = [question1, question2, question3]
+    //now we need to create a function that call the question over and over agian.
+    function nextQuestion() {
+
+        //store the question in an array
+       
+        //we create a randome selector. Then mutiply by our questionArray length
+        let randomSelector = Math.floor(Math.random() * questionArray.length)
+        //now we need to use randomselector on our array and use our displayQuestion from the 
+        //prototype from above
+        questionArray[randomSelector].displayQuestion()
+
+        //now we need to use  to change what they pick to a number
+        let answerAsk =prompt('Please select the correct answer.' + '  ' + ' To end the game, Type exit to quit game')
+
+      
+        /*check if answerAsk is not exit.
+        if it's not exit then it will go to our question array
+        inside question array it will pick one of them using the randomSelector
+        it will use the checkAnswer method we got from the prototype
+        so depend on what they pick we need to parse it to a number
+        Then we all so use the keepScore variable to update our score and keep our score
+        */
+        if (answerAsk !== 'exit') {
+            questionArray[randomSelector].checkAnswer(parseInt(answerAsk),(keepScore))
+            nextQuestion()
+        }
+
+    }
+    nextQuestion()
 })();
